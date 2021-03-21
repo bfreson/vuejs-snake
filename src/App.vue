a2933383354d9c732fad733c7a9d40d967f14584
Fréson Benjamin <benjamin.freson@gmail.com>
2021-03-21 01:32:00 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+     <h1> VueJS - Snake</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+   
+  },
+  data() {
+    return {
+     
+      
+    };
+  },
+  computed: {
+  
+  },
+  watch: {
+   
+  },
+  methods: {
+    
+  },
+  created() {
+   
+  },
+};
+</script>
+
+<style>
+
+</style>
